dc226d8a82ccc1ade2275f592925f8b3c6aa760c
wonhakchoi <wonhakchoi@gmail.com>
2022-09-02 00:09:35 -0700

    created template for song pages; made a Song class in models.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import uvicorn
 import credentials
 import spotify_funcs
+import models
 
 app = FastAPI()
 
@@ -37,10 +38,29 @@ async def post_home(request: Request, song_query: str = Form(...), artist_query:
     return RedirectResponse(url=f"/song/{song_id}", status_code=303)
 
 
-@app.get("/song/{song_id}")
-async def visualize_song(song_id):
-    return spotify.audio_features(song_id)[0], spotify.track(song_id)
+# @app.get("/song/{song_id}")
+# async def visualize_song(song_id):
+#     return spotify.audio_features(song_id)[0], spotify.track(song_id)
 
+@app.get("/song/{song_id}")
+async def get_song(request: Request, song_id: str):
+    track = spotify.track(song_id)
+    song_name = track['name']
+    artist_name = track['artists'][0]['name']
+    song_link = track['external_urls']['spotify']
+    cover_art = track['album']['images'][0]['url']
+
+    audio_features = spotify.audio_features(song_id)[0]
+    danceability = audio_features['danceability']
+    key = audio_features['key']
+    tempo = audio_features['tempo']
+    time_signature = audio_features['time_signature']
+
+    song = models.Song(
+        song_name=song_name, artist_name=artist_name, track_link=song_link, image=cover_art,
+        danceability=danceability, key=key, tempo=tempo, time_signature=time_signature
+    )
+    return templates.TemplateResponse("song.html", {"request": request, "song": song})
 
 if __name__ == "__main__":
     uvicorn.run(app)
---
+++ models.py
@@ -0,0 +1,13 @@
+from typing import Union
+from pydantic import BaseModel
+
+class Song(BaseModel):
+    song_name: str
+    artist_name: str
+    track_link: str
+    image: Union[str, None] = None
+    danceability: Union[float, None] = None
+    key: Union[int, None] = None
+    tempo: Union[float, None] = None
+    time_signature: Union[int, None] = None
+    

31a8350cb3ac037b23111466a9e2c9a8464dbc04
wesleychuu <wesleychu@telus.net>
2022-09-04 17:42:15 -0700

    fixed indentation in main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from email.mime import audio
-from fastapi import FastAPI, Request, Form 
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -34,7 +34,9 @@ async def get_home(request: Request):
 
 
 @app.post("/home", response_class=HTMLResponse)
-async def post_home(request: Request, song_query: str = Form(...), artist_query: str = Form(...)):
+async def post_home(
+    request: Request, song_query: str = Form(...), artist_query: str = Form(...)
+):
     song_id = spotify_funcs.get_song_id(song_query, artist_query)
     return RedirectResponse(url=f"/song/{song_id}", status_code=303)
 
@@ -43,30 +45,43 @@ async def post_home(request: Request, song_query: str = Form(...), artist_query:
 # async def visualize_song(song_id):
 #     return spotify.audio_features(song_id)[0], spotify.track(song_id)
 
+
 @app.get("/song/{song_id}")
 async def get_song(request: Request, song_id: str):
     track = spotify.track(song_id)
-    song_name = track['name']
-    artist_name = track['artists'][0]['name']
-    song_link = track['external_urls']['spotify']
-    cover_art = track['album']['images'][0]['url']
+    song_name = track["name"]
+    artist_name = track["artists"][0]["name"]
+    song_link = track["external_urls"]["spotify"]
+    cover_art = track["album"]["images"][0]["url"]
 
     audio_features = spotify.audio_features(song_id)[0]
-    key = audio_features['key']
-    tempo = audio_features['tempo']
-    time_signature = audio_features['time_signature']
-    acousticness = audio_features['acousticness']
-    danceability = audio_features['danceability']
-    energy = audio_features['energy']
-    instrumentalness = audio_features['instrumentalness']
-    liveness = audio_features['liveness']
-    valence = audio_features['valence']
-
+    key = audio_features["key"]
+    tempo = audio_features["tempo"]
+    time_signature = audio_features["time_signature"]
+    acousticness = audio_features["acousticness"]
+    danceability = audio_features["danceability"]
+    energy = audio_features["energy"]
+    instrumentalness = audio_features["instrumentalness"]
+    liveness = audio_features["liveness"]
+    valence = audio_features["valence"]
 
     song = models.Song(
-        song_name=song_name, artist_name=artist_name, track_link=song_link, image=cover_art, key=spotify_funcs.translate_key(int(key)), tempo=tempo, time_signature=time_signature, acousticness=acousticness, danceability=danceability, energy=energy, instrumentalness=instrumentalness, liveness=liveness, valence=valence
+        song_name=song_name,
+        artist_name=artist_name,
+        track_link=song_link,
+        image=cover_art,
+        key=spotify_funcs.translate_key(int(key)),
+        tempo=tempo,
+        time_signature=time_signature,
+        acousticness=acousticness,
+        danceability=danceability,
+        energy=energy,
+        instrumentalness=instrumentalness,
+        liveness=liveness,
+        valence=valence,
     )
     return templates.TemplateResponse("song.html", {"request": request, "song": song})
+
 
 if __name__ == "__main__":
     uvicorn.run(app)
